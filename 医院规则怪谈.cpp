4f706f4466545bf2df2f36fced36f66db1ff82d9
wleea <124417910+wleea@users.noreply.github.com>
2024-12-27 21:54:23 +0800

    Add files via upload
    Chapter One

---
+++ 医院规则怪谈.cpp
@@ -0,0 +1,412 @@
+//ÕÍ’˚∞Ê£°£°£°
+#include<bits/stdc++.h>
+#include<windows.h>
+#include<time.h>   
+#include<conio.h>
+int p;
+using namespace std;
+void tc();
+int shubiaolh(long long);
+void Color(int);
+void x23();
+int main()
+{
+   int x1,x2,x3;
+   system("color 0f");
+   cout<<"                        “Ω‘∫πÊ‘Úπ÷Ã∏"<<endl;
+   cout<<"                  Start(1)      exit(2)"<<endl;
+   cout<<">>";
+   cin>>p; 
+   
+   while(p!=1&&p!=2)
+   {
+   	 cout<<"∂ºÀµ¡À“™ ‰1∫Õ2£°1∫Õ2£°nm!"<<endl;
+   	 cout<<">>";
+   	 cin>>p;
+   }
+   if(p==2)
+   {
+   	 tc();
+   }
+   if(p==1)
+   {
+   	 Sleep(2300);
+   	 system("cls");
+   	 cout<<"ƒ„ «“ªº“–°“Ω‘∫µƒª§ ø..."<<endl; 
+   	 Sleep(2300); 
+   	 cout<<"“ªÃÏ£¨ƒ„’˝‘⁄÷µ“π∞‡..."<<endl; 
+	 Sleep(2300); 
+   	 cout<<" ±º‰:2024ƒÍ8‘¬3»’ 23:16 µÿµ„:“Ω‘∫◊°‘∫≤øƒ⁄ ÃÏ∆¯:?"<<endl; 
+   	 Sleep(2300); 
+   	 cout<<"ƒ„:∞° ∞°∞° ∞°∞°∞°  ≤√¥ ±∫Ú≤≈œ¬∞‡∞°..."<<endl;
+	 Sleep(2300); 
+   	 cout<<"“Ω…˙:’≈ª§ ø£¨¿¥Õ∆“ªœ¬2∫≈¥≤°£"<<endl; 
+   	 Sleep(2300); 
+   	 cout<<"ƒ„:¿¥¡À!"<<endl;
+   	 Sleep(2300); 
+   	 cout<<"(ª–„±º‰£¨ƒ„À∆∫ıÃ˝º˚¥≤…œÃ…◊≈µƒ≤°»ÀÀµ:»•4∫≈≤÷ø‚£°»•4∫≈≤÷ø‚£°)"<<endl;
+   	 Sleep(2300); 
+   	 cout<<"ƒ„: ≤√¥£ø"<<endl;
+   	 Sleep(2300); 
+   	 cout<<"“Ω…˙:‘ı√¥¡À£ø"<<endl;
+   	 Sleep(2300); 
+   	 cout<<"ƒ„:∞°....     (1)√ª ¬°£  (2)Œ“∫√œÒÃ˝º˚...”–»À‘⁄Ω–Œ“..."<<endl;
+   	 cout<<">>";
+	 cin>>x1; 
+	 if(x1==2)
+	 { 
+	 Sleep(2300); 
+   	 cout<<"“Ω…˙: «≤ª «¿€µΩ¡À£ø"<<endl;
+   	 Sleep(2300); 
+   	 cout<<"“Ω…˙:ƒ„ø…“™∫√∫√–›œ¢£¨≤ª“™Œ™¡À«Æ…À¡À…ÌÃÂ°£"<<endl;
+   	 Sleep(1000); 
+	 cout<<" ±º‰:2024ƒÍ8‘¬3»’ 23:22 µÿµ„:“Ω‘∫◊°‘∫≤ø≤°∑øƒ⁄ ÃÏ∆¯:?"<<endl;  
+   	 Sleep(2300); 
+   	 cout<<"(ƒ„√«“ª∆∞—¥≤Õ∆Ω¯¡À≤°∑ø...)"<<endl;
+   	 Sleep(2300); 
+   	 cout<<"“Ω…˙:∞—¥≤∑≈’‚¿Ô°£"<<endl;
+   	 Sleep(2300); 
+   	 cout<<"“Ω…˙:∫√£¨ƒ„ªÿ»•–›œ¢∞…°£"<<endl;
+   	 Sleep(2300); 
+   	 cout<<"ƒ„:∂˜£¨∫√°£"<<endl;
+   	 } 
+	 if(x1==1)
+	 { 
+	 Sleep(1000); 
+	 cout<<" ±º‰:2024ƒÍ8‘¬3»’ 23:22 µÿµ„:“Ω‘∫◊°‘∫≤ø≤°∑øƒ⁄ ÃÏ∆¯:?"<<endl; 
+	 cout<<"(ƒ„√«“ª∆∞—¥≤Õ∆Ω¯¡À≤°∑ø...)"<<endl;
+	 Sleep(2300); 
+   	 cout<<"“Ω…˙:∞—¥≤∑≈’‚¿Ô°£"<<endl;
+   	 Sleep(2300); 
+   	 cout<<"“Ω…˙:∫√£¨ƒ„ªÿ»•–›œ¢∞…°£"<<endl;
+   	 Sleep(2300); 
+   	 cout<<"ƒ„:∂˜£¨∫√°£"<<endl;
+   	 } 
+   	 Sleep(2300); 
+   	 cout<<"(ƒ„ø¥¡À“ª—€¬È“©æ¢ªπ√ªπ˝‘⁄¥≤…œÀØæıµƒ≤°»À...)"<<endl;
+   	 Sleep(2300);
+   	 cout<<"ƒ„:(À˚∏’≤≈’ÊµƒΩ–Œ“¡À¬£ø)"<<endl;
+   	 Sleep(2300);
+   	 cout<<"ƒ„:“™»•...4∫≈≤÷ø‚¬..."<<endl;
+   	 Sleep(2300);
+   	 cout<<"           »•(1)   ≤ª»•(2)"<<endl;
+   	 cout<<">>";
+	 cin>>x1;
+	 if(x1==2)
+	 {
+	 	cout<<"(ƒ„√ª”–»•°£)"<<endl;
+   	    Sleep(2300);
+   	    cout<<"ƒ„∂»π˝¡À“ª∏ˆÕÌ…œ£¨µΩ¡À‘Á…œ6:30,ƒ„œ¬∞‡¡À°£"<<endl;
+   	    Sleep(2300);
+   	    cout<<"                 ¥Ô≥…Ω·æ÷:∞≤»´∂»π˝"<<endl;
+   	    Sleep(2300);
+   	    cout<<"                      ”Œœ∑Ω· ¯£°"<<endl;
+   	    Sleep(2300);
+   	    cout<<"                      ∏––ª”ŒÕÊ£°"<<endl;
+   	    Sleep(2300);
+   	    return 0; 
+	 } 
+	 if(x1==1)
+	 {
+	 	cout<<"(ƒ„»•¡À4∫≈≤÷ø‚£¨◊ﬂ¡ÀΩ¯»•...)"<<endl;
+   	    Sleep(1500);
+	    cout<<" ±º‰:2024ƒÍ8‘¬3»’ 23:25 µÿµ„:“Ω‘∫4∫≈≤÷ø‚ ÃÏ∆¯:“ı"<<endl; 
+   	    cout<<"(’‚∏ˆ≤÷ø‚≤ª÷™ ≤√¥‘≠“ÚÕª»ª∑œ∆˙¡À...)"<<endl;
+   	    Sleep(1500);
+   	    cout<<"(ƒ„¥Úø™ ÷µÁÕ≤)"<<endl;
+   	    Sleep(1500);
+   	    cout<<"(µ∆π‚»√ƒ„µƒ–ƒ¿Ô”–¡À“ªµ„∞≤Œø°£)"<<endl;
+   	    Sleep(1500);
+   	    cout<<"(ƒ„◊ﬂµΩ¡À“ª∏ˆ«Ω±ﬂ)"<<endl;
+   	    Sleep(1500);
+   	    cout<<"(…œ√Ê”–“ª–©πÌª≠∑˚...)"<<endl;
+   	    Sleep(1500);
+   	    cout<<"(ƒ„ ‘◊≈∑≠“Î¡À“ªœ¬...)"<<endl;
+   	    Sleep(3000);
+   	    cout<<"(…œ√Ê–¥◊≈)"<<endl;
+   	    Sleep(1500);
+   	    system("cls");
+   	    Sleep(1500);
+   	    shubiaolh(300); 
+   	    cout<<"hi Œ“≤ª÷™µ¿ƒ„Œ™ ≤√¥¿¥µΩ¡À’‚¿Ô µ´ ««Îº«◊° ◊Ò ÿœ¬√ÊµƒπÊ‘Ú ƒ„≤≈ƒ‹ªÓœ¬»•"<<endl;
+   	    Sleep(2500);
+   	    cout<<"1.«Î≤ª“™œ‡–≈¥©¿∂∑€“¬∑˛µƒª§ ø"<<endl;
+   	    Sleep(2500);
+   	    cout<<"2.‘⁄24:00 ±£¨ø…ƒ‹ª·”–“ªŒª∆Úÿ§¿¥£¨ø¥º˚æ°¡ø≤ª“™”ÎÀ˚∂‘ ”£¨≤ª“™¿ÌÀ˚°£"<<endl;
+   	    Sleep(2500);
+   	    cout<<"3.÷ª”–18:00~20:00º“ Ù≤≈ø…“‘Ω¯»Î≤°∑ø£¨»Áπ˚‘⁄∆‰À˚ ±º‰∂Œø¥º˚¥©¿∂…´ÃıŒ∆ø„£¨…œ“¬∞◊¥Ûπ”µƒ»À£¨«Î¬˝¬˝Ã”◊ﬂ£¨—∞«Û“Ω…˙µƒ∞Ô÷˙°£≤ª“™¿ÌÀ˚°£"<<endl;
+   	    Sleep(2500);
+   	    Color(4); 
+   	    cout<<"4.∑≈–ƒ£¨’‚¿Ô√ª ≤√¥ø…“‘…ÀµΩƒ„"<<endl;
+   	    Sleep(2500);
+		Color(0); 
+   	    cout<<"ƒ„:(’‚ « ≤√¥£ø)"<<endl;
+   	    Sleep(2500);
+   	    cout<<"ƒ„:( «...∂Ò◊˜æÁ¬...)"<<endl;
+   	    Sleep(2500);
+   	    cout<<"ƒ„:(”¶∏√ «∞…...)"<<endl;
+   	    Sleep(2500);
+   	    system("cls");
+		Sleep(1000); 
+	    cout<<" ±º‰:2024ƒÍ8‘¬3»’ 23:59 µÿµ„:“Ω‘∫◊°‘∫≤øƒ⁄ ÃÏ∆¯:?"<<endl;  
+   	    cout<<"(ƒ„ªÿµΩ¡Àª§ øÃ®)"<<endl;
+   	    Sleep(2500);
+   	    cout<<"ƒ„:(”¶∏√√ª ¬°£)"<<endl;
+   	    Sleep(2500);
+   	    cout<<"(’‚ ±£¨ŒÁ“π12µ„µƒƒ÷÷”œÏ¡À...)"<<endl;
+   	    Sleep(2500);
+   	    cout<<"(√≈ø⁄≥ˆœ÷¡À“ª∏ˆ»À...)"<<endl;
+   	    Sleep(2500);
+   	    cout<<"          ◊ﬂ…œ«∞»•≤Èø¥(1)            ◊∞◊˜√ªø¥º˚(2)"<<endl; 
+   	    cout<<">>";
+   	    cin>>x2;
+   	    if(x2==1)
+   	    {
+   	      cout<<"(ƒ„◊ﬂ¡À…œ»•)"<<endl;
+   	      Sleep(2500);
+   	      cout<<"(∆Úÿ§Õª»ªªØ◊˜“ªµ¿∫⁄∆¯£¨Ω´ƒ„ÕÃ …¡À...)"<<endl;
+   	      shubiaolh(10000); 
+   	      Sleep(2500);
+   	      cout<<"ƒ„Œ•∑¥¡Àµ⁄2ÃıπÊ‘Ú..."<<endl;
+   	      Sleep(2500);
+   	      cout<<"                  ¥Ô≥…Ω·æ÷£∫À˚ «À≠£ø"<<endl;
+   	      Sleep(2500);
+   	      cout<<"                      ”Œœ∑Ω· ¯£°"<<endl;
+   	      Sleep(2300);
+   	      cout<<"                      ∏––ª”ŒÕÊ£°"<<endl;
+   	      Sleep(2300);
+   	      return 0; 
+   	  
+   	    }
+   	    if(x2==2)
+   	    {
+   	      cout<<"(ƒ„◊∞◊˜√ª”–ø¥º˚À˚...)"<<endl;
+   	      Sleep(2300);
+   	      cout<<"(π˝¡À“ªª·,À˚◊ﬂ¡À...)"<<endl;
+   	      Sleep(2300);
+   	      cout<<" ±º‰:2024ƒÍ8‘¬4»’ 00:37 µÿµ„:“Ω‘∫◊°‘∫≤øƒ⁄ ÃÏ∆¯:?"<<endl;
+   	      cout<<"“Ω…˙:’≈ª§ ø£¨∏˙Œ““ª∆»•»°∏ˆªı°£"<<endl; 
+   	      cout<<"                »•(1)      ≤ª»•(2)"<<endl; 
+   	      cout<<">>";
+   	      cin>>x2;
+   	      if(x2==3)
+   	      {
+   	      	x23();
+			  
+   	      }
+   	      if(x2==2)
+   	      {
+   	      	cout<<"ƒ„:∞° Œ“ªπ”– ¬ ƒ„œ»’“±»À∞…°£"<<endl;
+   	    	Sleep(2300);
+   	    	cout<<"(ƒ„π§◊˜¿€¡À£¨Õ˚œÚ¥∞Õ‚£¨‘⁄¥∞Õ‚”–“ª∏ˆ∆Æ∆Æ∫ˆ∫ˆµƒ”∞◊”£¨ƒ„∫‹“…ªÛ)"<<endl;
+   	    	Sleep(2300);
+   	    	cout<<"(Õª»ª£¨”∞◊”ªØ◊˜“ªµ¿∫⁄ŒÌ£¨Ω´ƒ„ÕÃ …°£)"<<endl; 
+			Sleep(2000);
+			cout<<"                    ƒ„Œ•∑¥¡Àµ⁄3ÃıπÊ‘Ú£°"<<endl; 
+   	    	cout<<"                    ¥Ô≥…Ω·æ÷:¥∞Õ‚»À"<<endl;
+   	    	Sleep(2300);
+   	    	cout<<"                      ”Œœ∑Ω· ¯£°"<<endl;
+   	    	Sleep(2300);
+   	    	cout<<"                      ∏––ª”ŒÕÊ£°"<<endl;
+   	    	Sleep(2300);
+   	    	return 0;
+   	      }
+   	      if(x2==1)
+		  {
+		  	cout<<"ƒ„:∞°...∫√°£"<<endl;
+   	    	Sleep(2300);
+   	    	cout<<"(ƒ„∏˙◊≈“Ω…˙“ª∆¿¥µΩ¡ÀÕ‚√Ê...)"<<endl;
+   	    	Sleep(1000); 
+   	    	cout<<" ±º‰:2024ƒÍ8‘¬4»’ 00:43 µÿµ„:“Ω‘∫ƒ⁄ ÃÏ∆¯:“ı"<<endl;
+   	    	cout<<"(ƒ„√«¿¥µΩ¡À“ª¡æ¥Ûø®≥µ«∞...)"<<endl;
+   	    	Sleep(2300);
+   	    	cout<<"¿≠ªı ¶∏µ:∞°£¨’‚∂— «ƒ„√«µƒ£¨∞—‘À∑—Ω·“ªœ¬æÕø…“‘ƒ√◊ﬂ¡À°£"<<endl;
+   	    	Sleep(2300);
+   	    	cout<<"(“Ω…˙÷ß∏∂¡À‘À∑—...)"<<endl;
+   	    	Sleep(2300);
+   	    	cout<<"(ƒ„√«∞—ªıƒ√¡Àªÿ»•...)"<<endl;
+   	    	Sleep(2300);
+   	    	cout<<"(‘⁄¿Îø™ ±£¨ƒ„ø¥º˚¡À“ª∏ˆ¥©¿∂…´ÃıŒ∆ø„£¨…œ“¬∞◊¥Ûπ”µƒ»À...)"<<endl;
+   	    	Sleep(3000);
+   	    	shubiaolh(10);
+   	    	system("cls");
+   	    	Sleep(1000);
+   	    	cout<<"Tie:÷ãÙ«  µÿµ„:GäF¡ÈàGÉÓÉÔÉ˘r GäFàGäF¡ÈàG∆¯:YâE?«tãs¸É?âu‰Îãu‰«E"<<endl;
+   	    	Sleep(100);
+   	    	system("cls");
+			cout<<" ±º‰:2024ƒÍ8‘¬4»’ 00:51 µÿµ„:“Ω‘∫◊°‘∫≤øƒ⁄ ÃÏ∆¯:?"<<endl;  
+			cout<<"(ƒ„∏˙“Ω…˙“ª∆ªÿµΩ¡À◊°‘∫≤ø)"<<endl; 
+			Sleep(2000);
+			cout<<"ƒ„:(∏’≤≈ƒ« « ≤√¥£ø)"<<endl; 
+			Sleep(2000);
+			cout<<"ƒ„:(“™∏ÊÀﬂ“Ω…˙¬£ø)"<<endl; 
+			Sleep(2000);
+			cout<<"                    ∏ÊÀﬂ(1)          ≤ª∏ÊÀﬂ(2)"<<endl; 
+			Sleep(2000);
+			cout<<">>";
+			cin>>x3;
+			if(x3==2)
+			{
+			  cout<<"(ƒ„√ª”–∏ÊÀﬂÀ˚°£)"<<endl; 
+			  Sleep(2000);
+			  cout<<"(2:00µƒ÷”…˘«√œÏ¡À...)"<<endl; 
+			  Sleep(2000);
+			  cout<<"(“ªπ…∫⁄ŒÌ≥Øƒ„œÆ¿¥£¨ƒ„±ª∫⁄ŒÌÕÃ …¡À°£)"<<endl; 
+			  Sleep(2000);
+			  cout<<"                     ƒ„Œ•∑¥¡Àµ⁄3ÃıπÊ‘Ú£°"<<endl; 
+   	    	  cout<<"                    ¥Ô≥…Ω·æ÷:ªπ «...≈‹≤ªµÙ∞°..."<<endl;
+   	    	  Sleep(2300);
+   	    	  cout<<"                      ”Œœ∑Ω· ¯£°"<<endl;
+   	    	  Sleep(2300);
+   	    	  cout<<"                      ∏––ª”ŒÕÊ£°"<<endl;
+   	    	  Sleep(2300);
+   	    	  return 0;
+			}
+			if(x3==1)
+			{
+			  cout<<"(ƒ„»´≤ø∏ÊÀﬂ¡ÀÀ˚...)"<<endl; 
+			  Sleep(2000);
+			  cout<<"“Ω…˙:∂˜...ø¥¿¥À˚”÷÷ÿœ÷»Àº‰¡À..."<<endl; 
+			  Sleep(2000);
+			  cout<<"(¥”“Ω…˙ø⁄÷–£¨ƒ„¡ÀΩ‚µΩ¡À’‚√¥“ª∏ˆπ  ¬...)"<<endl; 
+			  Sleep(2000);
+   	    	  cout<<"(‘⁄4ƒÍ«∞£¨”–“ªŒªæ´…Ò≤°ªº’ﬂ£¨‘⁄“Ω‘∫ƒ⁄ ‹∆€∏∫...)"<<endl;
+   	    	  Sleep(1500);
+   	    	  cout<<"(º∏∫ı√øÃÏ∂º“™±ª¥Ú...)"<<endl;
+   	    	  Sleep(1500);
+   	    	  cout<<"(À˚µƒ∞÷¬Ë±®æØ£¨∏˜÷÷…œÀﬂ£¨»¥∂º“‘ «º¶√´À‚∆§µƒ–° ¬ ‰¡À)"<<endl;
+   	    	  Sleep(1500);
+   	    	  cout<<"(π˝¡À“ª∂Œ ±º‰£¨À˚µƒ∞÷¬Ë±ª»Àƒ±…±¡À...)"<<endl;
+   	    	  Sleep(1500);
+   	    	  cout<<"(À˚“≤◊‘…±¡À£¨µ´ªπ «Õ¥∫ﬁ◊≈’‚◊˘“Ω‘∫£¨”⁄ «æÕ“ª÷±‘⁄’‚◊˘“Ω‘∫≈«ª≤...)"<<endl;
+   	    	  Sleep(1500);
+   	    	  cout<<"“Ω…˙:ƒ„÷Æ«∞ø¥µΩµƒπÊ‘Ú∆‰ µ «…œ“ªŒª±ªÀ˚…±À¿µƒ»À–¥µƒ..."<<endl;
+   	    	  Sleep(1500);
+   	    	  cout<<"(“Ω…˙Ω–¿¥¡À“‘«∞µƒµ¿ ø£¨◊º±∏∑‚”°À˚...)"<<endl;
+   	    	  Sleep(1500);
+   	    	  cout<<"     ºÃ–¯(1)    ◊Ë÷πÀ˚(2)"<<endl;
+   	    	  cout<<">>";
+   	    	  cin>>x3;
+   	    	  if(x3==1)
+   	    	  {
+   	    	  	cout<<"(µ¿ ø∞—À˚∑‚”°∆¿¥¡À...)"<<endl;
+				Sleep(1500);
+				cout<<"(ø… «£¨’‚—˘’Êµƒø…“‘¬£ø)"<<endl;
+				Sleep(1500);
+				cout<<"6ƒÍ∫Û..."<<endl;
+				Sleep(1500); 
+				cout<<" ±º‰:2030ƒÍ8‘¬6»’ 2:00 µÿµ„:“Ω‘∫◊°‘∫≤øƒ⁄ ÃÏ∆¯:?"<<endl;  
+				Sleep(1500); 
+				cout<<"“Ω…˙:’≈ª§ ø£¨∏˙Œ““ª∆»•»°∏ˆªı°£"<<endl; 
+				Sleep(2300); 
+				cout<<"ƒ„:∞° Œ“ªπ”– ¬ ƒ„œ»’“±»À∞…°£"<<endl;
+   	    		Sleep(2300);
+   	    		cout<<"(ƒ„π§◊˜¿€¡À£¨Õ˚œÚ¥∞Õ‚£¨‘⁄¥∞Õ‚”–“ª∏ˆ∆Æ∆Æ∫ˆ∫ˆµƒ”∞◊”£¨ƒ„∫‹“…ªÛ)"<<endl;
+   	    		Sleep(2300);
+   	    		cout<<"(Õª»ª£¨”∞◊”ªØ◊˜“ªµ¿∫⁄ŒÌ£¨Ω´ƒ„ÕÃ …°£)"<<endl; 
+				Sleep(2000);
+   	    		cout<<"              ¥Ô≥…◊Ó÷’Ω·æ÷2:’‚—˘...’Êµƒø…––¬£ø"<<endl;
+   	    		Sleep(2300);
+   	    		cout<<"                      ”Œœ∑Ω· ¯£°"<<endl;
+   	    		Sleep(2300);
+   	    		cout<<"                      ∏––ª”ŒÕÊ£°"<<endl;
+   	    		Sleep(2300);
+   	    		return 0;  
+   	    	  }
+   	    	  if(x3==2)
+   	    	  {
+   	    	  	cout<<"ƒ„:µ»“ªµ»"<<endl;
+				Sleep(1500);
+				cout<<"“Ω…˙&µ¿ ø:‡≈£ø"<<endl;
+				Sleep(1500);
+				cout<<"ƒ„:’‚—˘≤¢≤ªƒ‹Ω‚æˆÀ˚–ƒ¿Ô°£"<<endl;
+				Sleep(1500); 
+				cout<<"“Ω…˙&µ¿ ø:...»∑ µ"<<endl;  
+				Sleep(1500); 
+				cout<<"(“Ω…˙ø™µº÷–...)"<<endl; 
+				Sleep(2300); 
+				cout<<"“Ω…˙:...ƒ„“—æ≠∏ƒ±‰≤ª¡Àœ÷ µ¡À£¨Œ™ ≤√¥≤ª¥¯◊≈∆⁄¥˝»•ø™∆Ù–¬µƒ“ª…˙ƒÿ£ø"<<endl;
+   	    		Sleep(2300);
+   	    		cout<<"(À˚œ˚ ß≤ªº˚¡À...)"<<endl;
+   	    		Sleep(2300);
+   	    		cout<<"ƒ„:...‘Ÿº˚"<<endl; 
+				Sleep(2000);
+   	    		cout<<"                ¥Ô≥…◊Ó÷’Ω·æ÷1:...‘Ÿº˚"<<endl;
+   	    		Sleep(2300);
+   	    		cout<<"                      ”Œœ∑Ω· ¯£°"<<endl;
+   	    		Sleep(2300);
+   	    		cout<<"                      ∏––ª”ŒÕÊ£°"<<endl;
+   	    		Sleep(2300);
+   	    		return 0; 
+   	    	  }
+			} 
+		  }
+   	    }
+	 }
+   } 
+
+    return 0; 
+} 
+//∫Ø ˝≤ø∑÷£° 
+void tc()//ÕÀ≥ˆ  
+{
+  system("cls");
+   	 Sleep(500);
+   	 system("color f0");
+   	    cout<<endl<<endl<<endl<<endl<<"                        “Ω‘∫πÊ‘Úπ÷Ã∏    exit(2)"<<endl;
+        cout<<"                  Start(1)      "<<endl;
+    Sleep(1000);
+       	 system("cls");
+       	 system("color 1f");
+   	 Sleep(500);
+   	 system("color 0f");
+   	 cout<<endl<<endl<<"                                       exit(2)"<<endl;
+   	    cout<<endl<<endl<<endl<<endl<<"                        “Ω‘∫πÊ‘Úπ÷Ã∏"<<endl;
+        cout<<"                  Start(1)      "<<endl;
+    Sleep(1000);
+       	 system("cls");
+       	 system("color f0");
+   	 Sleep(500);
+       	 system("cls");
+       	 system("color 0f");
+   	 Sleep(500);
+   	 system("color 0f");
+   	 cout<<"                                       exit(2)"<<endl;
+   	    cout<<endl<<endl<<endl<<endl<<"                        “Ω‘∫πÊ‘Úπ÷Ã∏"<<endl;
+        cout<<"                  Start(1)      "<<endl;
+    Sleep(1000);
+       	 system("cls");
+       	 system("color c1");
+   	 Sleep(500);
+     shubiaolh(100000);
+   	 system("color 0f");
+   	    cout<<"                        “Ω‘∫πÊ‘Úπ÷Ã∏  "<<endl;
+        cout<<"                  Start(1)      "<<endl;
+        cout<<">>";
+     	cin>>p;
+}
+int shubiaolh(long long n)// Û±Í¬“ªŒ 
+{
+	int i;
+	int x=GetSystemMetrics(SM_CXSCREEN);   
+	int y=GetSystemMetrics(SM_CYSCREEN);//≥ı ºªØ Û±Í¬“ªŒ 
+for(i=0;i<n;i++)            //÷¥––n¥Œ! 
+	{ 
+	  for(int i=1;i<=2;i++) 
+	    SetCursorPos(rand()%y,rand()%x);// Û±Í¬“ªŒ 
+	}
+}
+void Color(int a)//—’…´ 
+{
+    if(a==0) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+    if(a==1) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_GREEN|FOREGROUND_BLUE);
+    if(a==2) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_GREEN);
+    if(a==3) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_BLUE);
+    if(a==4) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_RED);
+    if(a==5) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN);
+    if(a==6) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_BLUE);
+    if(a==7) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED|FOREGROUND_GREEN);
+    if(a==8) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
+    if(a==9) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|BACKGROUND_INTENSITY|BACKGROUND_RED);
+    if(a==10) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_GREEN|FOREGROUND_BLUE|BACKGROUND_INTENSITY|BACKGROUND_BLUE);}void x23(){system("cls");Color(4);cout<<"æØ±®:œµÕ≥∑¢œ÷õäLläD≥ˆœ÷¥ÌŒÛ£¨º¥Ω´«Â≥˝..."<<endl;cout<<"(ƒ„◊∞◊˜M—[J Ä  ?  ...)"<<endl;Sleep(2300);cout<<"(π˝¡À“ªª·,uıŸõäRichEõä◊ﬂ¡À...)"<<endl;Sleep(2300);cout<<" ±2h4ÉIÉGÇFÄFE~E{B{Ay@x@: ”ßg”¶f“¶f”¶f“•f—•f–§ µÿµ„:◊¨wKC Q)S+P [/^5†~eäTí\∆¯:6h5i6"<<endl;cout<<"“Ω…˙:h⁄—»” ¬‰ﬁ⁄‹’Œ∆∫ª§ ø£¨∏˙Rî^(uzè^ÂNûX†R“ª∆»•»°$éQ#çP$ãNäMµâQ∫ê[≤ÇF∏Å:¬â@¡á@≤y6¢~UüÉh⁄—»” ¬‰ﬁ⁄‹’Œ∆∫∞»º¥"<<endl; cout<<"                †RÔS(uÖQX[ˇ6qTçQ’ã (CFF~E)       ƒÀø∑˛(ïV&ú[)"<<endl; cout<<"??";Sleep(2000);cout<<"ƒ„À¿¡À..."<<endl;Sleep(1000);cout<<"¥Ô≥…“˛≤ÿΩ·æ÷:System Error"<<endl;Sleep(2000);cout<<"            ”Œœ∑Ω· ¯£°"<<endl;Sleep(2000);cout<<"            ∏––ª”ŒÕÊ£°"<<endl;}
+
